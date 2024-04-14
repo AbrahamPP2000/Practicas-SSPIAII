239a2e36b13266954a03d3e3697c3d9c8a3bdb4a
AbrahamPP2000 <abraham.ppinedo@alumnos.udg.mx>
2024-04-13 20:07:37 -0600

    Nuevo archivo py para la práctica 4

--- Practica4Colaborativo.py
+++ Practica4Colaborativo.py
@@ -1,2 +1,11 @@
 # Práctica 4 Red neuronal unicapa
 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
+from tkinter import filedialog
+
+paused = False
+
+
